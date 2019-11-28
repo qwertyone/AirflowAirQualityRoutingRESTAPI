2e4351714e4ef923fb59e744306770366d9e537c
Aaron G. (anotherhuman) <qwertyone@users.noreply.github.com>
2019-11-28 23:51:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 #from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from helper import gettrafficFlow, azureJSONmaparser
